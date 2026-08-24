a5a432c04b16f24dd40ec8e9ea4920227c5d8cfd
diyaz04 <166226880+diyaz04@users.noreply.github.com>
2026-08-24 11:26:21 +0700

    perbaruan

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,15 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: [
+          '"Plus Jakarta Sans Variable"',
+          '"Plus Jakarta Sans"',
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+        ],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
